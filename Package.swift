6918f0fbc3f71ed91e667a5942b1ea683fd57e68
Cristian Díaz <cristian@210x7.io>
2025-11-16 19:40:08 +0100

    Fix overlapping sources by removing test target for now

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-      .package(url: "https://github.com/Smith-Tools/smith-core", from: "1.0.0"),
+      .package(url: "https://github.com/Smith-Tools/smith-core", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
     ],
     targets: [
